b0f5515d5ac1a221b993b86049f092d09c2d2528
bigmosi <kinyeramo@gmail.com>
2022-06-01 15:42:53 +0300

    build functions using arrays to generate lists of items

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
-import './style.css';
+import "./index.css";
 
-console.log(12345);
+const todos = [
+    {
+        index: 1,
+        description: 'Buy food stuff',
+        completed: false
+    },
+    {
+        index: 2,
+        description: 'Complete all microverse activity',
+        completed: false,
+    },
+    {
+        index: 3,
+        description: 'Watch Tv program',
+        completed: false,
+    },
+    {
+        index: 4,
+        description: 'Eat super',
+        completed: false,
+    }
+];
+
+const todoList = () => {
+    const list = document.querySelector('.todo-list');
+
+    todos.forEach((todo) => {
+        list.innerHTML += `
+        <div class="main-container">        
+        <div class="list-item">
+        <input type='checkbox' class="checkbox" id=${todo.index} />
+        <input type='text' class='desc font-normal px-1' value='${todo.description}' disabled/>
+        <span class='fa fa-ellipsis-v fa-lg'></span>
+        </div>
+       </div> 
+        `;
+    });
+}
+
+todoList();
--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,34 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
-    mode: 'development',
-    entry: {
-        bundle: path.resolve(__dirname, 'src/index.js'),
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name][contenthash].js',
-        clean: true,
-    },
-    devtool: 'source-map',
-    devServer: {
-        static: {
-            directory: path.resolve(__dirname, 'dist'),
-        },
-        port: 3000,
-        open: true,
-        hot: true,
-        compress: true,
-        historyApiFallback: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader', 'sass-loader'],
-            },
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: 'src/template.html',
-        }),
+  mode: "development",
+  entry: {
+    index: "./src/index.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html'), 
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
     ],
-};
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+    clean: true,
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 8000,
+    open: true,
+    hot: true,
+  },
+}
