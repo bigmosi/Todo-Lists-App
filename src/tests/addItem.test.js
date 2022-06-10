bb9c9845bcda3786a75445b846fed46cbc4c89f0
abdulhamiid <adioabdulhamid@yahoo.com>
2022-06-10 15:41:06 +0100

    Add test for completed task

--- src/tests/addItem.test.js
+++ src/tests/addItem.test.js
@@ -44,3 +44,31 @@ describe('Todo list removeItem tests', () => {
     expect(list).toHaveLength(0);
   });
 });
+
+describe('set completed status', () => {
+  test('test mark completed', () => {
+    taskstore.setTaskStaus(0);
+    taskstore.setTaskStaus(1);
+    taskstore.printTodoList();
+    const statusOne = document.querySelectorAll('.todo-list li input[type=checkbox]')[0].checked;
+    const statusTwo = document.querySelectorAll('.todo-list li input[type=checkbox]')[1].checked;
+    expect(statusOne).toBe(true);
+    expect(statusTwo).toBe(true);
+  });
+});
+describe('clear completed tasks', () => {
+  test('clear all todos', () => {
+    taskstore.removeTask(true, -1);
+    let list = document.querySelectorAll('.todo-list li');
+    expect(list).toHaveLength(0);
+    taskstore.addTask('go swimming');
+    taskstore.addTask('go hiking');
+    taskstore.addTask('prepare dinner');
+    taskstore.setTaskStaus(0);
+    taskstore.setTaskStaus(1);
+    taskstore.setTaskStaus(2);
+    taskstore.removeTask(true, -1);
+    list = document.querySelectorAll('.todo-list li');
+    expect(list).toHaveLength(0);
+  });
+});
